bce80b7f567b463839579af61399d6bef8b4e74d
mlau11 <mlau448@gmail.com>
2020-01-31 14:59:38 -0500

    template for app

---
+++ BadgeWatch/BadgeWatch/BadgeWatch/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BadgeWatch.Models;
+
+namespace BadgeWatch.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
