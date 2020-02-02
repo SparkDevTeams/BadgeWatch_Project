3b7ae29d1d3df9c3a5c30df27e5289eae3273e2a
mlau11 <55466213+mlau11@users.noreply.github.com>
2020-02-02 18:29:28 -0500

    added geocoding but page has not appeared on the app

--- BadgeWatch/BadgeWatch/BadgeWatch.iOS/Main.cs
+++ BadgeWatch/BadgeWatch/BadgeWatch.iOS/Main.cs
@@ -4,13 +4,14 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Xamarin.Essentials;
 
 namespace BadgeWatch.iOS
 {
     public class Application
     {
         // This is the main entry point of the application.
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
