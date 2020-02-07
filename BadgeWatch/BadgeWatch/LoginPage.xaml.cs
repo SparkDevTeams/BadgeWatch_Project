6a61b3ffd5899db302b7b696acdb41364712680e
Jessie Cotton <jjc11895@gmail.com>
2020-02-07 15:22:48 -0500

    folder push

---
+++ BadgeWatch/BadgeWatch/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BadgeWatch
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLogin(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new BadgeWatch.View.MainMasterDetailPage());
+        }
+    }
+}
