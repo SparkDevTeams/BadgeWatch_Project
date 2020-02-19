ebfc4879d21c5c7c99f8bc3bcae6be428238b795
awor001 <awors001@fiu.edu>
2020-02-19 14:16:24 -0500

    Downloading Errors
    I was trying to install the things neededn for this project. I can't run anything so far even after downloading 300+ files.

--- BadgeWatch/BadgeWatch/View/NewsFeed.xaml.cs
+++ BadgeWatch/BadgeWatch/View/NewsFeed.xaml.cs
@@ -7,8 +7,94 @@ using System.Xml;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Social;
+
+
 namespace BadgeWatch.View
 {
+    static void Main(string[] args)
+        {
+            // Replace the following placeholder value with the target server URL.
+            const string serverUrl = "http://serverName/";
+
+            Console.Write("Type your post text:  ");
+
+            // Create a link to include in the post.
+            SocialDataItem linkDataItem = new SocialDataItem();
+            linkDataItem.ItemType = SocialDataItemType.Link;
+            linkDataItem.Text = "link";
+            linkDataItem.Uri = "http://bing.com";
+
+            // Define properties for the post.
+            SocialPostCreationData postCreationData = new SocialPostCreationData();
+            postCreationData.ContentText = Console.ReadLine() + " Plus a {0}.";
+            postCreationData.ContentItems = new SocialDataItem[1] { linkDataItem };
+
+            // Get the client context.
+            ClientContext clientContext = new ClientContext(serverUrl);
+
+            // Get the SocialFeedManager instance.
+            SocialFeedManager feedManager = new SocialFeedManager(clientContext);
+
+            // Publish the post. This is a root post, so specify null for the
+            // targetId parameter. 
+            feedManager.CreatePost(null, postCreationData); 
+            clientContext.ExecuteQuery();
+
+            Console.WriteLine("\\nCurrent user's newsfeed:");
+
+            // Set parameters for the feed content that you want to retrieve.
+            SocialFeedOptions feedOptions = new SocialFeedOptions();
+
+            // Get the target owner's feed and then run the request on the server.
+            ClientResult<SocialFeed> feed = feedManager.GetFeed(SocialFeedType.News, feedOptions);
+            clientContext.ExecuteQuery();
+
+            // Create a dictionary to store the Id property of each thread. This 
+            // code example stores the Id so you can select a thread to reply to.
+            Dictionary<int, string> idDictionary = new Dictionary<int, string>();
+
+            // Iterate through each thread in the feed.
+            for (int i = 0; i < feed.Value.Threads.Length; i++)
+            {
+                SocialThread thread = feed.Value.Threads[i];
+
+                // Keep only the threads that can be replied to.
+                if (thread.Attributes.HasFlag(SocialThreadAttributes.CanReply))
+                {
+                    idDictionary.Add(i, thread.Id);
+
+                    // Get properties from the root post and thread.
+                    // If a thread contains more than two replies, the server returns
+                    // a thread digest that contains only the two most recent replies.
+                    // To get all replies, call SocialFeedManager.GetFullThread.
+                    SocialPost rootPost = thread.RootPost;
+                    SocialActor author = thread.Actors[rootPost.AuthorIndex];
+                    Console.WriteLine(string.Format("{0}. {1} said \\"{2}\\" ({3} replies)",
+                        (i + 1), author.Name, rootPost.Text, thread.TotalReplyCount));
+                }
+            }
+            Console.Write("\\nWhich thread number do you want to reply to?  ");
+
+            string threadToReplyTo = "";
+            int threadNumber = int.Parse(Console.ReadLine()) - 1;
+            idDictionary.TryGetValue(threadNumber, out threadToReplyTo);
+
+            Console.Write("Type your reply:  ");
+
+            // Define properties for the reply. This example reuses the 
+            // SocialPostCreationData object that was used to create a post.
+            postCreationData.ContentText = Console.ReadLine();
+
+            // Publish the reply and make the changes on the server.
+            ClientResult<SocialThread> result = feedManager.CreatePost(threadToReplyTo, postCreationData);
+            clientContext.ExecuteQuery();
+
+            Console.WriteLine("\\nThe reply was published. The thread now has {0} replies.", result.Value.TotalReplyCount);
+            Console.ReadLine();
+         }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewsFeed : ContentPage
     {
